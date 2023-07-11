4a735481f351b3683ebf73a5070580a0d6dc5df7
Daniel Clover <danil.word@mail.ru>
2023-07-11 17:29:45 +0500

    BegOfAddingUI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotesComponent } from './pages/notes/notes.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children: [
+    {path: '', component: NotesComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesComponent } from './pages/notes/notes.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
