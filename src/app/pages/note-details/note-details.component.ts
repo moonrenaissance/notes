46d3954280eef9bab8fd759a62b226527030141b
Daniel Clover <danil.word@mail.ru>
2023-07-17 20:31:15 +0500

    UpdRouts

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Note } from 'src/app/models/note.model';
 
 @Component({
@@ -11,13 +12,17 @@ export class NoteDetailsComponent implements OnInit{
   
   note: Note;
   
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.note = new Note();
   }
 
   onSubmit(form: NgForm) {
-    
+    this.router.navigateByUrl('/');
+  }
+
+  Cancel() {
+    this.router.navigateByUrl('/');
   }
 }
