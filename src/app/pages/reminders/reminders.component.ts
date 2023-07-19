5615bbbe265cd3722d6d42dd90636639ca02b8d7
illager10 <georgiy.pe4kin@mail.ru>
2023-07-19 14:14:07 +0500

    changes in main page and services

---
+++ src/app/pages/reminders/reminders.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reminders',
+  templateUrl: './reminders.component.html',
+  styleUrls: ['./reminders.component.scss']
+})
+export class RemindersComponent {
+
+}
