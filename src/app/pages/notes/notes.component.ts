3696d28f52f50c13c1bfecaa673554562a847ff8
illager10 <georgiy.pe4kin@mail.ru>
2023-07-21 15:43:05 +0500

    add filter changes

--- src/app/pages/notes/notes.component.ts
+++ src/app/pages/notes/notes.component.ts
@@ -24,8 +24,19 @@ export class NotesComponent implements OnInit{
     .subscribe({
       next: (notes) =>{
         console.log(notes);
-        this.notes = notes.filter(n=> n.date.toString() == '1899-12-30T19:57:27');
-        console.log(new Date(0,0,0,0,0));
+        this.notes = notes;
+        this.filteredNotes = notes;
+      },
+      error: (response)=>{
+        console.log(response);
+      }
+    });
+
+    this.tagsService.getAllTags()
+    .subscribe({
+      next: (tag) =>{
+        console.log(tag);
+        this.tags = tag;
       },
       error: (response)=>{
         console.log(response);
